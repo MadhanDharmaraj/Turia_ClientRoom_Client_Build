d1d86d7388a27d7946fc2330c58e081125c0bf99
Madhan Dharmaraj <madhan@turia.in>
2024-06-29 10:12:06 +0530

    api url

---
+++ static/js/24.9732747b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[24],{1066:function(a,s,d){}}]);
+//# sourceMappingURL=24.9732747b.chunk.js.map
